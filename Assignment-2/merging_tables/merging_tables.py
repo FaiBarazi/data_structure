87c3f3c2425bb1ccfeca2057f6933a17d78f388c
FaiBarazi <f.barazi@gmail.com>
2018-04-28 20:44:27 -0400

    Implement disjoint sets with print statements for naive debugging

--- Assignment-2/merging_tables/merging_tables.py
+++ Assignment-2/merging_tables/merging_tables.py
@@ -6,26 +6,31 @@ n, m = map(int, sys.stdin.readline().split())
 lines = list(map(int, sys.stdin.readline().split()))
 rank = [1] * n
 parent = list(range(0, n))
-ans = max(lines)
 
 def getParent(table):
-    # find parent and compress path
+    """Equivalent to find set, uses path compression. """
+    if table != parent[table]:
+        table = getParent(parent[table])
     return parent[table]
 
 def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
-
-    if realDestination == realSource:
-        return False
+    root_destination, root_source = getParent(destination), getParent(source)
+    print('I am destination', root_destination + 1)
+    print('I am source', root_source + 1)
+    if root_destination != root_source:
+        parent[source] = destination
+        rank[root_destination] += 1
+        lines[root_destination] += lines[root_source]
+        print('I am lines', lines)
+    print('I am the parent list', parent)
 
     # merge two components
-    # use union by rank heuristic 
+    # use union by rank heuristic
     # update ans with the new maximum table size
-    
+
     return True
 
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
     merge(destination - 1, source - 1)
-    print(ans)
-    
+    print(max(lines))
